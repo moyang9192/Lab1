b96c47e291f781fd5d382cd92e0389cbf234f10e
moyang9192 <moyang9192@gmail.com>
2016-08-31 14:37:55 -0400

    Lab1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,12 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab1
+namespace CSharp_Lab1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
+            Console.WriteLine("");
+            string cont;
+            do
+
+
+            {
+                Console.WriteLine("Enter the Length for Rectangle:");
+                float a = float.Parse(Console.ReadLine());
+                Console.WriteLine("");
+
+                Console.WriteLine("Enter the Width for Rectangle:");
+                float b = float.Parse(Console.ReadLine());
+                Console.WriteLine("");
+
+                Console.WriteLine("Area of rectangle is : " + a * b);
+
+                Console.WriteLine("Perimeter of rectangle is: " + (2 * a + 2 * b));
+                Console.ReadLine();
+                Console.WriteLine("");
+
+                Console.WriteLine("Do you want to continue? (y/n)");
+                cont = Console.ReadLine();
+                Console.WriteLine("");
+
+            } while (cont == "y");
+            Console.WriteLine("");
+
+            Console.WriteLine("Goodbye!");
+            Console.ReadLine();
+
+
         }
     }
 }
